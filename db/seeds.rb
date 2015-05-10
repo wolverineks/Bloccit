4dfc93477134345c13dfc696107f6ca6a478c49f
wolverine.ks <wolverine.ks@gmail.com>
2015-05-10 14:17:47 -0700

    Created advertisement model, controller, routes. Seeded database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,35 @@ require 'faker'
 
 #Create Posts |
 50.times do 
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-  )
-end
-posts = Post.all 
+   Advertisement.create!(
+     title: Faker::Lorem.sentence,
+     copy:  Faker::Lorem.paragraph,
+     price: Faker::Commerce.price
+   )
+ end
 
-100.times do 
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-  )
-end
+ ads = Advertisement.all 
+
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} ads created"
+
+#Create Posts |
+# 50.times do 
+#   Post.create!(
+#     title: Faker::Lorem.sentence,
+#     body:  Faker::Lorem.paragraph
+#   )
+# end
+# posts = Post.all 
+
+# 100.times do 
+#   Comment.create!(
+#     post: posts.sample,
+#     body: Faker::Lorem.paragraph
+#   )
+# end
+
+# puts "Seed finished"
+# puts "#{Post.count} posts created"
+# puts "#{Comment.count} comments created"
