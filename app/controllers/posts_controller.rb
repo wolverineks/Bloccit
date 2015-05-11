d16fb528b9823f35a0884d064dcbe212f059286f
wolverine.ks <wolverine.ks@gmail.com>
2015-05-11 10:29:57 -0700

    Defined the new method for posts_controller. Inserted html for posts.new view. Defined the create method for posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,23 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body))
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
