3c51ab2470cbec0a4167046ac6e9d35273fb1d43
wolverine.ks <wolverine.ks@gmail.com>
2015-05-17 21:24:02 -0700

    Added scopes to Post class. Tested scope: passed.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('created_at ASC') }
+
+  scope :ordered_by_title, -> { order('title DESC') }
+
+  scope :ordered_by_reverse_created_at, -> { order('created_at DESC') }
+
 end
