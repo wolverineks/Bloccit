50d9ac637f8b771c7698484480e0c32ddd4dcdec
wolverine.ks <wolverine.ks@gmail.com>
2015-05-19 16:28:03 -0700

    fixed redirect bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
     authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error updating the post. Please try again."
       render :edit
