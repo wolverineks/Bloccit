4814f576aa6b860af8350af3b834a7ade7702664
wolverine.ks <wolverine.ks@gmail.com>
2015-05-20 14:14:00 -0700

    started topics validation - WIP

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,22 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  def markdown_title
+    markdown_to_html(title)
+  end
+
+  def markdown_body
+    markdown_to_html(body)
+  end
+
+  private
+
+  def markdown_to_html(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
 end
