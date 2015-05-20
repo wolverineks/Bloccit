8b45e95224f11813b9d87670c6bd113391c1810c
wolverine.ks <wolverine.ks@gmail.com>
2015-05-20 09:17:55 -0700

    Added Post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ActiveRecord::Base
 
   default_scope { order('created_at ASC') }
 
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 end
