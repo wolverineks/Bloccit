d6baf6a61a48ac7feb5ed19d142cc62518d53807
wolverine.ks <wolverine.ks@gmail.com>
2015-05-23 17:22:51 -0700

    Uploading Images - WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522184906) do
+ActiveRecord::Schema.define(version: 20150522223831) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150522184906) do
     t.integer  "user_id"
     t.integer  "topic_id"
     t.integer  "summary_id"
+    t.string   "image"
   end
 
   add_index "posts", ["summary_id"], name: "index_posts_on_summary_id"
@@ -90,6 +91,7 @@ ActiveRecord::Schema.define(version: 20150522184906) do
     t.datetime "updated_at"
     t.string   "role",                   default: "Guest"
     t.string   "avatar"
+    t.string   "avatara"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
