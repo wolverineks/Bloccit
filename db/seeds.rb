585cfbb60ca1fd5640b668fa6ab3222bbc677856
wolverine.ks <wolverine.ks@gmail.com>
2015-06-01 12:10:51 -0700

    destroy comleted

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,31 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'faker'
-
+ 
+ 
 # Create Topics
 5.times do 
+  tmp_file = open(Faker::Avatar.image)
+  FileUtils.mv tmp_file, '/tmp/avatar.png'
+  
   topic = Topic.create!(
     name:        Faker::Lorem::sentence,
     description: Faker::Lorem.paragraph,
-    image:       Faker::Avatar.image
+    image:       File.open('/tmp/avatar.png') 
   )
 end
 topics = Topic.all
 
 # Create Users |
 5.times do 
+  tmp_file = open(Faker::Avatar.image)
+  FileUtils.mv tmp_file, '/tmp/avatar.png'
+
   user = User.new(
     name:     Faker::Name.name,
     email:    Faker::Internet.email,
     password: Faker::Lorem.characters(10),
-    avatar:   Faker::Avatar.image
+    avatar:   File.open('/tmp/avatar.png') 
   )
   user.skip_confirmation!
   user.save!
@@ -32,18 +39,21 @@ end
 users = User.all
 
 # Create Posts |
-50.times do 
+5.times do 
+  tmp_file = open(Faker::Avatar.image)
+  FileUtils.mv tmp_file, '/tmp/avatar.png'
+
   Post.create!(
     user:  users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph,
     topic: topics.sample,
-    image: Faker::Avatar.image
+    image: File.open('/tmp/avatar.png') 
   )
 end
 posts = Post.all 
 
-5000.times do 
+5.times do 
   Comment.create!(
     user: users.sample,
     post: posts.sample,
@@ -52,30 +62,44 @@ posts = Post.all
 end
 
  # Create an admin user
+ tmp_file = open(Faker::Avatar.image)
+ FileUtils.mv tmp_file, '/tmp/avatar.png'
+
  admin = User.new(
    name:     'Admin User',
    email:    'admin@example.com',
    password: 'helloworld',
-   role:     'admin'
+   role:     'admin',
+   avatar:   File.open('/tmp/avatar.png') 
+
  )
  admin.skip_confirmation!
  admin.save!
  
  # Create a moderator
+ tmp_file = open(Faker::Avatar.image)
+ FileUtils.mv tmp_file, '/tmp/avatar.png'
+
  moderator = User.new(
    name:     'Moderator User',
    email:    'moderator@example.com',
    password: 'helloworld',
-   role:     'moderator'
+   role:     'moderator',
+   avatar:    File.open('/tmp/avatar.png') 
+
  )
  moderator.skip_confirmation!
  moderator.save!
  
  # Create a member
+ tmp_file = open(Faker::Avatar.image)
+ FileUtils.mv tmp_file, '/tmp/avatar.png'
+
  member = User.new(
    name:     'Member User',
    email:    'member@example.com',
-   password: 'helloworld'
+   password: 'helloworld',
+   avatar:    File.open('/tmp/avatar.png') 
  )
  member.skip_confirmation!
  member.save!
