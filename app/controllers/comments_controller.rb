9c4ad7cba7d7c3792ac69b80309d7b6fcade0cda
wolverine.ks <wolverine.ks@gmail.com>
2015-06-01 09:31:00 -0700

    Comments Destroy Completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,15 @@
 class CommentsController < ApplicationController
 
-  # def index
-    
-  # end
-
-  # def show
-    
-  # end
-
   def create
     @post = Post.find(params[:post_id])
-    
+    @topic = @post.topic
     @comment = Comment.new(comment_params.merge(post: @post, user: current_user))
-    
     @comments = @post.comments.page(params[:page]).per_page(10)
 
     authorize @comment
 
-    if @comment.save
-      redirect_to @post.topic.post
+    if @comment.save 
+     redirect_to [@topic, @post]
       
     else
       flash[:error] = "There was an error saving the comment. Please try again."
@@ -27,24 +18,17 @@ class CommentsController < ApplicationController
 
   end
 
-  # def edit
-
-  # end
-
-  # def update
-
-  # end
-
   def destroy
     @post = Post.find(params[:post_id])
+    @topic = @post.topic
     @comment = @post.comments.find(params[:id])
     authorize @comment
 
     if @comment.destroy
-      redirect_to [@post]
+      redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error deleting the comment. Please try again."
-      render [@post] 
+      render [@topic, @post] 
     end
   end
 
