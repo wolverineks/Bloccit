894c641ab88e23c73344585d9d2d8f74fd60f6ca
wolverine.ks <wolverine.ks@gmail.com>
2015-06-02 13:37:31 -0700

    added Vote test

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,13 +5,15 @@ describe Vote do
     
     before do
       @post = Post.create(title: 'post title', body: 'post body')
-      3.times { @post.votes.create(values: 1) }
+      3.times { @post.votes.create(value: 1) }
       2.times { @post.votes.create(value: -1) }
     end
 
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect( @post.votes.each ).to eq(1 || -1) 
+        expect( @post.votes[0] == (1||-1) ).eql? true 
+        expect( @post.votes[1] == (1||-1) ).eql? true 
+        expect( @post.votes[2] == (1||-1) ).eql? true 
       end
     end
   
