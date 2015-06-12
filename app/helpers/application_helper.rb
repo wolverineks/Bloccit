1099fbc233ba6e4d6de20edd2850e4fddd72501d
wolverine.ks <wolverine.ks@gmail.com>
2015-06-12 10:39:05 -0700

    refactored up/down vote links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,19 @@ module ApplicationHelper
      end
    end
 
-   def markdown_to_html(markdown)
+  def markdown_to_html(markdown)
     renderer = Redcarpet::Render::HTML.new
     extensions = {fenced_code_blocks: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
+  end
+
+  def up_vote_link_classes(post)
+    (current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : ''
+  end
+
+  def down_vote_link_classes(post)
+    (current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' 
   end
 
 end
