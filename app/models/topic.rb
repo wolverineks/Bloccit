2639c99afd881d9b6c59b9cd08918e4e65931ebf
wolverine.ks <wolverine.ks@gmail.com>
2015-06-15 16:33:06 -0700

    added topicspec

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,11 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  
+  scope :visible_to, -> (user) { user ? all : publicly_viewable }
+
+  scope :publicly_viewable, -> { where(public: true)  }
+
+  scope :privately_viewable, -> { where(public: false) }
 
   mount_uploader :image, ImageUploader
 
