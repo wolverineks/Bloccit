943e6d100a3dd80e95d44fbf3c59de850d0a43be
wolverine.ks <wolverine.ks@gmail.com>
2015-06-17 16:07:38 -0700

    finished ajax destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,20 +20,22 @@ class CommentsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:post_id])
-    @topic = @post.topic
     @comment = @post.comments.find(params[:id])
     authorize @comment
 
     if @comment.destroy
-      redirect_to [@topic, @post]
+      flash[:notice] = "Comment was removed."
     else
       flash[:error] = "There was an error deleting the comment. Please try again."
-      render [@topic, @post] 
     end
   end
 
   def comment_params
     params.require(:comment).permit(:body)
   end
-
+  
+  respond_to do |format|
+    format.html
+    format.js
+  end
 end
